00a46ff5329f3696ba783cc12238da94e833d024
Nikolai Kudasov <nickolay.kudasov@gmail.com>
2022-05-28 21:18:32 +0300

    Print Lambda code too

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,10 @@
 module Main where
 
-import qualified Fun.Compile    as Fun
-import qualified GRIN.Pretty    as GRIN
-import qualified Lambda.Compile as Lam
-import qualified Syntax.Fun.Par as Fun
+import qualified Fun.Compile         as Fun
+import qualified GRIN.Pretty         as GRIN
+import qualified Lambda.Compile      as Lam
+import qualified Syntax.Fun.Par      as Fun
+import qualified Syntax.Lambda.Print as Lam
 
 main :: IO ()
 main = do
@@ -13,5 +14,9 @@ main = do
     Left err -> print err
     Right program -> do
       let programLam = Fun.compileProgram program
+      putStrLn "==================================================="
+      putStrLn (Lam.printTree programLam)
+      putStrLn "==================================================="
       let programGRIN = Lam.compileProgram programLam
       putStrLn (GRIN.ppGRIN programGRIN)
+      putStrLn "==================================================="
